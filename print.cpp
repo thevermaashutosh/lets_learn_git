bc474197353f162e5fd02f1e10e9173be667cd34
Ashutosh Verma <ashutosh.verma.mec21@iitbhu.ac.in>
2022-06-25 12:13:59 +0530

    print.cpp _:P

--- print.cpp
+++ print.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 int32_t main(){
-    int a, b; string s = "Sum of the numbers = ";
-    cin>>a>>b; cout<<s<<a+b<<"\n******";
+    int a, b; string s = "Sum of the numbers = \n";
+    cin>>a>>b; cout<<s<<a+b<<"\n******\n";
+    cout<<pow(2, 3)<<" "<<sqrt(144)<<"\n";
 }
